395f1e9b711bdb9b400fd4b841324791c52b65d1
David van Leusen <jvleusen@gmail.com>
2014-08-22 21:32:35 +0200

    Pre-emptively set preferIPv4Stack to avoid the download stalling issue.

--- src/main/java/net/serverpeon/twitcharchiver/Bootstrap.java
+++ src/main/java/net/serverpeon/twitcharchiver/Bootstrap.java
@@ -4,6 +4,8 @@ import net.serverpeon.twitcharchiver.ui.GUI;
 
 public class Bootstrap {
     public static void main(final String[] args) throws Exception {
+        System.setProperty("java.net.preferIPv4Stack", "true");
+
         new GUI().init();
     }
 }
